1301c36924b43099596666a683c66cf8bec1ad66
urvanov <hedgehogues@bk.ru>
2018-03-15 01:18:13 +0300

    Выпилен DocumentReader

--- internal/io/reader.py
+++ internal/io/reader.py
@@ -15,39 +15,18 @@ class ImageFromPDFReader:
         self.converter.convert_all()
 
     """
+        Returns the path to the last image was read
+
+        Возвращает путь к последнему прочитанному изображению
+    """
+    def get_path(self):
+        return self.converter.get_path()
+
+    """
         Метод возвращает следующее изображение из пула
     """
     def read(self):
         return cv2.imread(self.converter.next_path_to_image(), cv2.IMREAD_GRAYSCALE)
-
-
-class DocumentFromPDFReader:
-    """
-        Подокументное читение изображений, подготовленных pdf-конвертором
-    """
-
-    """
-        converter: конвертер, у которого есть метод convert_all() для выполнения конвертации всех pdf в изображения и
-                   метод next_image(), возвращающий следующее изображение
-    """
-    def __init__(self, converter):
-        self.converter = converter
-        self.converter.convert_all()
-
-        self.paths = []
-        self.last_path = 0
-
-    """
-        Метод возвращает следующее изображение из пула
-    """
-    def read(self):
-        if len(self.paths) == self.last_path:
-            self.paths = self.converter.next_paths_to_documents()
-            self.last_path = 0
-        else:
-            self.last_path += 1
-
-        return cv2.imread(self.paths[self.last_path], cv2.IMREAD_GRAYSCALE)
 
 
 class ImageReader:
